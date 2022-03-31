03de962bef61a0247b17ef11dc1e151c6cae4bca
JavAndy58 <digital.designs.russia@gmail.com>
2022-03-31 17:37:22 +0300

    Создание проекта Todo #2

---
+++ src/main/java/ru/job4j/todo/model/Item.java
@@ -0,0 +1,76 @@
+package ru.job4j.todo.model;
+
+//import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+
+//@Entity
+public class Item {
+
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String description;
+    private LocalDateTime created = LocalDateTime.now().truncatedTo(ChronoUnit.MICROS);
+    private boolean done;
+
+    public Item() {
+    }
+
+    public Item(int id, String description, LocalDateTime created, boolean done) {
+        this.id = id;
+        this.description = description;
+        this.created = created;
+        this.done = done;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        return id == item.id && done == item.done && Objects.equals(description, item.description) && Objects.equals(created, item.created);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, description, created, done);
+    }
+}
