600250c44616bdf294931b10076a635e899567ba
JavAndy58 <digital.designs.russia@gmail.com>
2022-05-17 16:26:39 +0300

    3. Лямбды и шаблон wrapper. [#49295]

--- src/main/java/ru/job4j/todo/store/ItemStore.java
+++ src/main/java/ru/job4j/todo/store/ItemStore.java
@@ -2,9 +2,11 @@ package ru.job4j.todo.store;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.Item;
 import java.util.List;
+import java.util.function.Function;
 
 @Repository
 public class ItemStore {
@@ -16,80 +18,82 @@ public class ItemStore {
     }
 
     public List<Item> findAll() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List result = session.createQuery("from ru.job4j.todo.model.Item").list();
-        session.getTransaction().commit();
-        return result;
+        return this.tx(
+                session -> session.createQuery("from ru.job4j.todo.model.Item").list()
+        );
     }
 
     public Item findById(Integer id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Item result = session.get(Item.class, id);
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return this.tx(
+                session -> session.get(Item.class, id)
+        );
     }
 
     public void updateById(Integer id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.createQuery("update Item i set i.done = true where i.id = :fId")
-                        .setParameter("fId", id)
-                        .executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+         this.tx(
+                session -> {
+                    session.createQuery("update Item i set i.done = true where i.id = :fId")
+                            .setParameter("fId", id)
+                            .executeUpdate();
+                    return new Object();
+                }
+        );
     }
 
     public void deleteById(Integer id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.createQuery("delete from Item where id = :fId")
-                .setParameter("fId", id)
-                .executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+        this.tx(
+                session -> {
+                    session.createQuery("delete from Item where id = :fId")
+                            .setParameter("fId", id)
+                            .executeUpdate();
+                    return new Object();
+                }
+        );
     }
-    
+
     public List<Item> findCompleted() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List result = session.createQuery("from Item where done = true").list();
-        session.getTransaction().commit();
-        return result;
+        return this.tx(
+                session -> session.createQuery("from Item where done = true").list()
+        );
     }
 
     public List<Item> findNew() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List result = session.createQuery("from Item i order by i.created desc").list();
-        session.getTransaction().commit();
-        return result;
+        return this.tx(
+                session -> session.createQuery("from Item i order by i.created desc").list()
+        );
     }
 
     public Item create(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.save(item);
-        session.getTransaction().commit();
-        session.close();
-        return item;
+        return this.tx(
+                session -> {
+                    session.save(item);
+                    return item;
+                }
+        );
     }
 
     public void update(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.update(item);
-        session.getTransaction().commit();
-        session.close();
+        this.tx(
+                session -> {
+                    session.update(item);
+                    return new Object();
+                }
+        );
+
     }
 
-    public void deleted(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.delete(item);
-        session.getTransaction().commit();
-        session.close();
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sf.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
     }
 }
