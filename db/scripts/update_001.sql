b4ff0e199a803868d576e4f64045ec19932344b0
JavAndy58 <digital.designs.russia@gmail.com>
2022-07-07 17:54:16 +0300

    4. Категории в TODO List [#331991 #318205]

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -14,3 +14,20 @@ CREATE TABLE IF NOT EXISTS items
     done BOOLEAN,
     user_id INT NOT NULL REFERENCES users(id)
 );
+
+CREATE TABLE IF NOT EXISTS categories
+(
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE items_categories
+(
+    item_id INT NOT NULL REFERENCES items(id),
+    categories_id INT NOT NULL REFERENCES categories(id)
+);
+
+INSERT INTO categories (name) VALUES ('Работа');
+INSERT INTO categories (name) VALUES ('Семья');
+INSERT INTO categories (name) VALUES ('Образование');
+INSERT INTO categories (name) VALUES ('Личное');
--- db/scripts/update_002.sql
+++ db/scripts/update_002.sql
@@ -1,10 +1,2 @@
-CREATE TABLE IF NOT EXISTS categories
-(
-    id SERIAL PRIMARY KEY,
-    name TEXT
-);
 
-INSERT INTO categories (name) VALUES ('Работа');
-INSERT INTO categories (name) VALUES ('Семья');
-INSERT INTO categories (name) VALUES ('Образование');
-INSERT INTO categories (name) VALUES ('Личное');
+
